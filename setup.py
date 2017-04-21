bd17075b760cdd2e3982129884abe7c50034faab
Marco Fiscato <marco@swifkey.com>
2017-04-21 16:40:29 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='DeepLearnTute',
+      version='0.5',
+      packages=['dlt'],
+      package_data={'dlt': ['custom_input.js']})
